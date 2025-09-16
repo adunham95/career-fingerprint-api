809733d1073cbd49f4d2437e276727ddcd6f3001
Adrian <adunham95@gmail.com>
2025-09-16 12:45:48 -0400

    Added session cookies add in

--- src/authcookie/authcookie.service.ts
+++ src/authcookie/authcookie.service.ts
@@ -3,7 +3,7 @@ import { Response } from 'express';
 
 @Injectable()
 export class AuthCookieService {
-  setAuthCookie(response: Response, accessToken: string) {
+  setAuthCookie(response: Response, accessToken: string, sessionID?: string) {
     response.cookie('accessToken', accessToken, {
       httpOnly: true,
       secure: process.env.NODE_ENV === 'production',
@@ -11,10 +11,26 @@ export class AuthCookieService {
       maxAge: 1000 * 60 * 60 * 24 * 7, // 7 days
       domain: process.env.COOKIE_DOMAIN,
     });
+    if (sessionID) {
+      response.cookie('sessionAccessToken', sessionID, {
+        httpOnly: true,
+        secure: process.env.NODE_ENV === 'production',
+        sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax',
+        maxAge: 1000 * 60 * 60 * 24 * 7, // 7 days
+        domain: process.env.COOKIE_DOMAIN,
+      });
+    }
   }
 
   clearAuthCookie(response: Response) {
     response.clearCookie('accessToken', {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === 'production',
+      sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax',
+      domain: process.env.COOKIE_DOMAIN,
+      maxAge: -1,
+    });
+    response.clearCookie('sessionAccessToken', {
       httpOnly: true,
       secure: process.env.NODE_ENV === 'production',
       sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax',

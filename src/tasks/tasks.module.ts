53fe04b71d14bd02351d21875258b3999b3b532f
Adrian <adunham95@gmail.com>
2026-04-25 19:31:13 -0400

    Updated to create a deployment queue for emails

--- src/tasks/tasks.module.ts
+++ src/tasks/tasks.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { ScheduleModule } from '@nestjs/schedule';
+import { BullModule } from '@nestjs/bull';
 import { TasksService } from './tasks.service';
 import { TasksController } from './tasks.controller';
+import { TasksProcessor } from './tasks.processor';
 import { PrismaModule } from 'src/prisma/prisma.module';
 import { MailModule } from 'src/mail/mail.module';
 import { AchievementModule } from 'src/achievement/achievement.module';
@@ -10,12 +12,13 @@ import { LoginTokenModule } from 'src/login-token/login-token.module';
 @Module({
   imports: [
     ScheduleModule.forRoot(),
+    BullModule.registerQueue({ name: 'tasks' }),
     PrismaModule,
     MailModule,
     AchievementModule,
     LoginTokenModule,
   ],
   controllers: [TasksController],
-  providers: [TasksService],
+  providers: [TasksService, TasksProcessor],
 })
 export class TasksModule {}
